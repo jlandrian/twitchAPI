e6b2fbb126a82f7354341277cef23b63163fa95e
jlandrian <jlandrian@gmail.com>
2018-01-13 17:09:42 -0800

    working properly

--- twitch.js
+++ twitch.js
@@ -1,140 +1,45 @@
 $(document).ready(function(){
-   var streams = [
-      {
-        "stream": {
-          "mature": false,
-          "status": "Greg working on Electron-Vue boilerplate w/ Akira #programming #vuejs #electron",
-          "broadcaster_language": "en",
-          "display_name": "FreeCodeCamp",
-          "game": "Creative",
-          "language": "en",
-          "_id": 79776140,
-          "name": "freecodecamp",
-          "created_at": "2015-01-14T03:36:47Z",
-          "updated_at": "2016-09-17T05:00:52Z",
-          "delay": null,
-          "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png",
-          "banner": null,
-          "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-channel_offline_image-b8e133c78cd51cb0-1920x1080.png",
-          "background": null,
-          "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_banner-6f5e3445ff474aec-480.png",
-          "profile_banner_background_color": null,
-          "partner": false,
-          "url": "https://www.twitch.tv/freecodecamp",
-          "views": 161989,
-          "followers": 10048,
-          "_links": {
-            "self": "https://api.twitch.tv/kraken/channels/freecodecamp",
-            "follows": "https://api.twitch.tv/kraken/channels/freecodecamp/follows",
-            "commercial": "https://api.twitch.tv/kraken/channels/freecodecamp/commercial",
-            "stream_key": "https://api.twitch.tv/kraken/channels/freecodecamp/stream_key",
-            "chat": "https://api.twitch.tv/kraken/chat/freecodecamp",
-            "subscriptions": "https://api.twitch.tv/kraken/channels/freecodecamp/subscriptions",
-            "editors": "https://api.twitch.tv/kraken/channels/freecodecamp/editors",
-            "teams": "https://api.twitch.tv/kraken/channels/freecodecamp/teams",
-            "videos": "https://api.twitch.tv/kraken/channels/freecodecamp/videos"
-          }
-        },
-        "_links": {
-          "self": "https://api.twitch.tv/kraken/streams/freecodecamp",
-          "channel": "https://api.twitch.tv/kraken/channels/freecodecamp"
-        }
-      },
-      {
-        "stream": null,
-        "display_name": "OgamingSC2",
-        "_links": {
-          "self": "https://api.twitch.tv/kraken/streams/ogamingsc2",
-          "channel": "https://api.twitch.tv/kraken/channels/ogamingsc2"
-        }
-      },
-      {
-        "stream": {
-          "mature": false,
-          "status": "RERUN: StarCraft 2 - Kane vs. HuK (ZvP) - WCS Season 3 Challenger AM - Match 4",
-          "broadcaster_language": "en",
-          "display_name": "ESL_SC2",
-          "game": "StarCraft II",
-          "language": "en",
-          "_id": 30220059,
-          "name": "esl_sc2",
-          "created_at": "2012-05-02T09:59:20Z",
-          "updated_at": "2016-09-17T06:02:57Z",
-          "delay": null,
-          "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_image-d6db9488cec97125-300x300.jpeg",
-          "banner": null,
-          "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-channel_offline_image-5a8657f8393c9d85-1920x1080.jpeg",
-          "background": null,
-          "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_banner-f8295b33d1846e75-480.jpeg",
-          "profile_banner_background_color": "#050506",
-          "partner": true,
-          "url": "https://www.twitch.tv/esl_sc2",
-          "views": 60843789,
-          "followers": 135275,
-          "_links": {
-            "self": "https://api.twitch.tv/kraken/channels/esl_sc2",
-            "follows": "https://api.twitch.tv/kraken/channels/esl_sc2/follows",
-            "commercial": "https://api.twitch.tv/kraken/channels/esl_sc2/commercial",
-            "stream_key": "https://api.twitch.tv/kraken/channels/esl_sc2/stream_key",
-            "chat": "https://api.twitch.tv/kraken/chat/esl_sc2",
-            "subscriptions": "https://api.twitch.tv/kraken/channels/esl_sc2/subscriptions",
-            "editors": "https://api.twitch.tv/kraken/channels/esl_sc2/editors",
-            "teams": "https://api.twitch.tv/kraken/channels/esl_sc2/teams",
-            "videos": "https://api.twitch.tv/kraken/channels/esl_sc2/videos"
-          }
-        },
-        "_links": {
-          "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
-          "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
-        }
-      },
-      {
-        "stream": null,
-        "display_name": "noobs2ninjas",
-        "_links": {
-          "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
-          "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
-        }
-      },
-      {
-        "error": "Not Found",
-        "status": 404,
-        "message": "Channel 'not-a-valid-account' does not exist"
-      }
-    ];
-
-    var html = "";
+    var API = "https://wind-bow.glitch.me/twitch-api";
+    var users = "/users/";
+    var channels = "/channels/";
+    var streams = "/streams/";
     
-    function showStream() {
-        //loop through the array
-        streams.forEach(function(data){
-            //if there is a stream value, then do something
+    var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
             
-            if(data.stream) {
-                html += "<div class='row'><img class='rounded-circle' src='" +
-                    data.stream.logo + "' alt='logo'>";
-                console.log(data.stream.status);
+    streamers.forEach(function(streamer){
+        
+        
+        $.getJSON(API + streams + streamer, function(data){  
+            var logo, name, status;
+            var row = "<div class='row rounded-right' id='" + streamer + " '>";
+            
+            if(data.stream){
+                name = data.stream.channel.display_name;
+                logo = data.stream.channel.logo;
+                status = data.stream.channel.status;
+                
+                row += "<div class='col-md-4 float-left'><img class='rounded-circle' src='" + logo + "' alt='logo'></div>";
+                row += "<div class='col-md-4'>" + status + "</div>";
+                row += "<div class='col-md-4'>" + name + "</div>";
+                $(".container").append(row);
+          
             } else {
-                console.log("no data!");
-            }
-            html += "</div><br>"
-            $(".table-dark").html(html);
-            //get the keys of each object
-            
-//           var keys = Object.keys(data);
-//            console.log(keys[1]);
-//            //add a div with class 'twitch'
-//            html += "<div class='twitch'>";
-//            //run another forEach on the keys object
-//            keys.forEach(function(innerData){
-//                html += "<strong>" + innerData + "</strong>" + data[innerData]+ "<br>";
-//            });
-//            html += "</div><br>"
-//            $("body").html(html);
-//        });
-    });
-}
+              
+              $.getJSON(API + users + streamer, function(data2){
+                name = data2.display_name;
+                logo = data2.logo;
+                status = "OFFLINE";
 
-showStream();
-    
+                row += "<div class='col-md-4'><img class='rounded-circle' src='" + logo + "' alt='logo'></div>";
+                row += "<div class='col-md-4'>" + status + "</div>";
+                row += "<div class='col-md-4'>" + name + "</div>";
+                $(".container").append(row);            
+              });
+          }
+            
+            
+           
+        });
+        
+    });    
 });
